c17fc71d2e372e6ec235ace261732d6d8a3e66bd
Kan <38980663+KanTheAstronaut@users.noreply.github.com>
2023-09-17 23:51:16 +0400

    Update Photino.Mac.mm

--- Photino.Native/Photino.Mac.mm
+++ Photino.Native/Photino.Mac.mm
@@ -180,6 +180,7 @@ Photino::Photino(PhotinoInitParams* initParams)
 
     // FIX AUTOPLAY ISSUES
     [_webviewConfiguration.preferences setValue: @NO forKey: @"requiresUserGestureForVideoPlayback"];
+    _webviewConfiguratio.preferences.mediaTypesRequiringUserActionForPlayback = WKAudiovisualMediaTypeNone;
     _webviewConfiguration.mediaTypesRequiringUserActionForPlayback = WKAudiovisualMediaTypeNone;
 
     //wire up custom url schemes
